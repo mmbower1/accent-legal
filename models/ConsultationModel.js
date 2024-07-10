c0675b8538c737078e524f3eb780aac12e6efe65
mmbower1 <mttbwr91@gmail.com>
2024-07-09 22:22:02 -0700

    styling fixes and consultation schema backend

--- models/ConsultationModel.js
+++ models/ConsultationModel.js
@@ -4,26 +4,58 @@ const consultationSchema = mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please add a name."],
-    unique: true,
   },
   email: {
     type: String,
     required: [true, "Please add an email."],
-    unique: true,
     match: [
       /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
       "Please add valid email.",
     ],
   },
-  phone: {},
+  phone: {
+    type: String,
+    required: [true, "Please add a phone number"],
+  },
   message: {
     type: String,
     required: [true, "Please add a description."],
-    unique: true,
   },
-  checkboxes: {},
+  monday: {
+    type: Boolean,
+    default: false,
+  },
+  tuesday: {
+    type: Boolean,
+    default: false,
+  },
+  wednesday: {
+    type: Boolean,
+    default: false,
+  },
+  thursday: {
+    type: Boolean,
+    default: false,
+  },
+  friday: {
+    type: Boolean,
+    default: false,
+  },
+  morning: {
+    type: Boolean,
+    default: false,
+  },
+  afternoon: {
+    type: Boolean,
+    default: false,
+  },
+  evening: {
+    type: Boolean,
+    default: false,
+  },
   uscis: {
     type: Boolean,
+    default: false,
   },
   createdAt: {
     type: Date,
--- routes/consultation.js
+++ routes/consultation.js
@@ -1,8 +1,52 @@
 const express = require("express");
 const router = express.Router();
+const Consultation = require("../models/ConsultationModel");
+// middleware
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/asyncHandler");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ success: true, msg: "Consultation route" });
-});
+// @desc GET all consultations
+// @route GET /consultation
+// @access Public
+router.get(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const consultations = await Consultation.find();
+    res.status(200).json({
+      success: true,
+      count: consultations.length,
+      data: consultations,
+    });
+  })
+);
+
+// @desc GET single consultation
+// @route GET /consultation/:id
+// @access Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res, next) => {
+    const consultation = await Consultation.findById(req.params.id);
+    if (!consultation)
+      return next(
+        new ErrorResponse(
+          `Consultation not found with ID of ${req.params.id}`,
+          404
+        )
+      );
+    res.status(200).json({ success: true, data: consultation });
+  })
+);
+
+// @desc POST a new consultation
+// @route POST /consultation
+// @access Public
+router.post(
+  "/",
+  asyncHandler(async (req, res, next) => {
+    const consultation = await Consultation.create(req.body);
+    res.status(201).json({ success: true, data: consultation });
+  })
+);
 
 module.exports = router;
